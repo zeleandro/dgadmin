b01e2951cd9b1f2227e4e5f446ab9c123fd7b4f0
Leandro <pallud_leandro@hotmail.com>
2020-11-16 23:58:03 -0300

    contact

--- src/views/about/index.js
+++ src/views/about/index.js
@@ -8,69 +8,69 @@ import image5 from '../../../static/image5.jpg'
 class About extends Component {
     render() {
         return (
-                <div class="container app-content  my-3 my-md-5">
-					<div class="side-app">
-						<div class="page-header">
+                <div className="container app-content  my-3 my-md-5">
+					<div className="side-app">
+						<div className="page-header">
 
 						</div>
 
-						<div class="">
-							<div class="card">
-								<div class="row">
-									<div class="col-md-12 col-lg-6 pr-0 d-none d-lg-block">
-										<img src={image1} alt="img" class="br-tl-2 br-bl-2 "></img>
+						<div className="">
+							<div className="card">
+								<div className="row">
+									<div className="col-md-12 col-lg-6 pr-0 d-none d-lg-block">
+										<img src={image1} alt="img" className="br-tl-2 br-bl-2 "></img>
 									</div>
-									<div class="col-md-12 col-lg-6  pl-0 ">
-										<div class="card-body p-7 about-con pabout">
-											<h2 class="mb-4 font-weight-semibold">Por que {this.props.empresa}?</h2>
-											<h4 class="leading-normal">majority have suffered alteration in some form, by injected humour</h4>
-											<p class="leading-normal">There are many variations of passages of Lorem Ipsum available, but the majority have suffered  by injected humour, or randomised words which don't look even slightly believable.
+									<div className="col-md-12 col-lg-6  pl-0 ">
+										<div className="card-body p-7 about-con pabout">
+											<h2 className="mb-4 font-weight-semibold">Por que {this.props.empresa}?</h2>
+											<h4 className="leading-normal">majority have suffered alteration in some form, by injected humour</h4>
+											<p className="leading-normal">There are many variations of passages of Lorem Ipsum available, but the majority have suffered  by injected humour, or randomised words which don't look even slightly believable.
 											If you are going to use a passage of Lorem Ipsum, you need to as necessary All the Lorem Ipsum generators on the Internet tend to repeat</p>
 										</div>
 									</div>
 								</div>
 							</div>
-							<div class="row">
-								<div class="col-md-12 cols-ms-12 col-lg-4">
-									<div class="card text-center">
-										<img src={image2} alt="img" class="br-tl-2 br-tr-2" ></img>
-										<div class="card-body">
-											<h3 class="mb-3">Historia</h3>
+							<div className="row">
+								<div className="col-md-12 cols-ms-12 col-lg-4">
+									<div className="card text-center">
+										<img src={image2} alt="img" className="br-tl-2 br-tr-2" ></img>
+										<div className="card-body">
+											<h3 className="mb-3">Historia</h3>
 											<p>I must explain to you how all this mistaken idea of denouncing pleasure and you a complete account of the system</p>
 										</div>
 									</div>
 								</div>
-								<div class="col-md-12 cols-ms-12 col-lg-4">
-									<div class="card text-center">
-										<img src={image3} alt="img" class="br-tl-2 br-tr-2" ></img>
-										<div class="card-body">
-											<h3 class="mb-3">Sobre {this.props.nombre}</h3>
+								<div className="col-md-12 cols-ms-12 col-lg-4">
+									<div className="card text-center">
+										<img src={image3} alt="img" className="br-tl-2 br-tr-2" ></img>
+										<div className="card-body">
+											<h3 className="mb-3">Sobre {this.props.nombre}</h3>
 											<p>I must explain to you how all this mistaken idea of denouncing pleasure and you a complete account of the system</p>
 										</div>
 									</div>
 								</div>
-								<div class="col-md-12 cols-ms-12 col-lg-4">
-									<div class="card text-center">
-										<img src={image4} alt="img" class="br-tl-2 br-tr-2" ></img>
-										<div class="card-body">
-											<h3 class="mb-3">Crecimiento</h3>
+								<div className="col-md-12 cols-ms-12 col-lg-4">
+									<div className="card text-center">
+										<img src={image4} alt="img" className="br-tl-2 br-tr-2" ></img>
+										<div className="card-body">
+											<h3 className="mb-3">Crecimiento</h3>
 											<p>I must explain to you how all this mistaken idea of denouncing pleasure and you a complete account of the system</p>
 										</div>
 									</div>
 								</div>
 							</div>
-							<div class="card">
-								<div class="row">
-									<div class="col-md-12 col-lg-6  pr-0 ">
-										<div class="card-body p-7 pabout">
-											<h2 class="mb-4 font-weight-semibold">Que ofrecemos?</h2>
-											<h4 class="leading-normal">majority have suffered alteration in some form, by injected humour</h4>
-											<p class="leading-normal">There are many variations of passages of Lorem Ipsum available, but the majority have suffered  by injected humour, or randomised words which don't look even slightly believable.
+							<div className="card">
+								<div className="row">
+									<div className="col-md-12 col-lg-6  pr-0 ">
+										<div className="card-body p-7 pabout">
+											<h2 className="mb-4 font-weight-semibold">Que ofrecemos?</h2>
+											<h4 className="leading-normal">majority have suffered alteration in some form, by injected humour</h4>
+											<p className="leading-normal">There are many variations of passages of Lorem Ipsum available, but the majority have suffered  by injected humour, or randomised words which don't look even slightly believable.
 											If you are going to use a passage of Lorem Ipsum, you need to as necessary All the Lorem Ipsum generators on the Internet tend to repeat</p>
 										</div>
 									</div>
-									<div class="col-md-12 col-lg-6 pl-0 d-none d-lg-block">
-										<img src={image5} alt="img" class=" br-br-2 br-tr-2"></img>
+									<div className="col-md-12 col-lg-6 pl-0 d-none d-lg-block">
+										<img src={image5} alt="img" className=" br-br-2 br-tr-2"></img>
 									</div>
 								</div>
 							</div>
--- src/views/contact/index.js
+++ src/views/contact/index.js
@@ -1,17 +1,68 @@
 import React, { Component } from 'react';
 
-class About extends Component {
+class Contact extends Component {
     render() {
         return (
-            <div class="container app-content  my-3 my-md-5">
-                <div class="side-app">
-                    <div class="page-header">
-                        <h2>Contacto</h2>
-                    </div>
+            <div className="container app-content  my-3 my-md-5">
+                <div className="side-app">
+                    <section className="sptb bg-white">
+                        <div className="container">
+                            <div className="section-title center-block text-center">
+                                <h1>Información de Contacto</h1>
+                                <p>En que podemos ayudarlo?</p>
+                            </div>
+                            <div className="support">
+                                <div className="row text-white">
+                                    <div className="col-xl-4 col-lg-12 col-md-12">
+                                        <div className="support-service bg-dark br-2 mb-4 mb-xl-0">
+                                            <i className="fab fa-instagram"></i>
+                                            <h6>Instagram</h6>
+                                            <a className="social-icon text-dark" href="http://www.instagram.com/dg.clean"
+                                            target="_blank">@dg.clean</a>
+                                        </div>
+                                    </div>
+                                    <div className="col-xl-4 col-lg-12 col-md-12">
+                                        <div className="support-service bg-success br-2 mb-4 mb-xl-0">
+                                            <i className="fab fa-facebook-square"></i>
+                                            <h6>Facebook</h6>
+                                            <a className="social-icon text-dark" href="http://www.facebook.com/DGClean"
+                                            target="_blank">@DGClean</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="support">
+                                <div className="row text-white">
+                                    <div className="col-xl-4 col-lg-12 col-md-12">
+                                        <div className="support-service bg-primary br-2 mb-4 mb-xl-0">
+                                            <i className="fa fa-phone"></i>
+                                            <h6>+54 362-422-3010</h6>
+                                            <p>Envienos sus dudas!</p>
+                                        </div>
+                                    </div>
+                                    <div className="col-xl-4 col-lg-12  col-md-12">
+                                        <div className="support-service bg-secondary br-2 mb-4 mb-xl-0">
+                                            <i className="fas fa-envelope"></i>
+                                            <h6>diego_grnja@hotmail.com</h6>
+                                            <p>Contactenos!</p>
+                                        </div>
+                                    </div>
+                                    <div className="col-xl-4 col-lg-12 col-md-12">
+                                        <div className="support-service bg-warning br-2 mb-4 mb-xl-0">
+                                            <i className="fas fa-map-marked"></i>
+                                            <h6>Orquideas 385 - Resistencia</h6>
+                                            <p>Envíos sin cargo</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+
                 </div>
             </div>
         )
     }
 }
 
-export default About
+export default Contact
