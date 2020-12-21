882a0e895481841a86c0f2a136cb624c56f4d82f
blas.geat <blasc147@gmail.com>
2020-12-21 20:47:35 -0300

    tarjetas y doble carga

--- src/components/category/CategoryItem.jsx
+++ src/components/category/CategoryItem.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
 import { useDispatch } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+import { useHistory , Link} from 'react-router-dom';
 import ImageLoader from '../ui/ImageLoader';
 import img from '../../../static/j1.png'
 
@@ -13,21 +13,23 @@ const CategoryItem = ({
 }) => {
 	const dispatch = useDispatch();
 	const history = useHistory();
+	// const onClickItem = () => {
+	// 	if (isLoading) return;
 
-	const onClickItem = () => {
-		if (isLoading) return;
-
-		if (category.id) {
-			history.push(`/products/${category.name}`);
-		}
-	};
+	// 	if (category.id) {
+	// 		history.push(`/products/${category.name}`);
+	// 	}
+	// };
 
 	return (
 		<div className="col-xl-3  col-md-6 col-xs-12">
 			<div className={`card ${!category.id ? 'product-loading' : ''}`}>
 						<div className="item-card">
 							<div className="item-card-desc">
-								<a href="" onClick={onClickItem}></a>
+								<Link to={{
+								pathname: `/products/${category.name}`,
+
+								}} />
 								<div className="item-card-img">
 									<img src={img} alt="img" className="br-tr-7 br-tl-7" />
 								</div>
