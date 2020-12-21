d5b72318a60655e4722d20a7b459ae2dad3401a1
Leandro <pallud_leandro@hotmail.com>
2020-12-20 21:27:31 -0300

    Persistor for products

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -8,6 +8,8 @@ import storage from 'redux-persist/lib/storage';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer from '../reducers';
 import rootSaga from '../sagas/rootSaga';
+// import hardSet from 'redux-persist/lib/stateReconciler/hardSet';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 
 const sagaMiddleware = createSagaMiddleware();
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -15,7 +17,8 @@ const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const authPersistConfig = {
 	key: 'root',
 	storage,
-	whitelist: ['auth', 'profile', 'basket', 'checkout']
+	stateReconciler: autoMergeLevel2,
+	whitelist: ['auth', 'profile', 'basket', 'checkout', 'products']
 };
 
 export default () => {
