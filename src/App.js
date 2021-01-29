38d7ca8767f2b15573ba4028a30d30a28a98a47b
blas.geat <blasc147@gmail.com>
2021-01-29 12:14:59 -0300

    ga

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React, { StrictMode } from 'react';
+import React, { StrictMode, useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import AppRouter from 'routers/AppRouter';
 import Preloader from 'components/ui/Preloader';
 
-const App = ({ store, persistor }) => (
-	<StrictMode>
-		<Provider store={store}>
-			<PersistGate loading={<Preloader />} persistor={persistor}>
-				<AppRouter />
-			</PersistGate>
-		</Provider>
-	</StrictMode>
-);
+import ReactGA from 'react-ga';
+ReactGA.initialize('G-8EW9Q8WFDQ');
+
+
+const App = ({ store, persistor }) => {
+			useEffect(()=>{
+				ReactGA.pageview(window.location.pathname + window.location.search);
+			})
+			return(
+				<StrictMode>
+					<Provider store={store}>
+						<PersistGate loading={<Preloader />} persistor={persistor}>
+							<AppRouter />
+						</PersistGate>
+					</Provider>
+				</StrictMode>
+)};
 
 export default App;
